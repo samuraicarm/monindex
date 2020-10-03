870091c9227090381a22ef979c61736336f5d497
Carmen Dukes <carmendukes@gmail.com>
2020-10-03 15:29:22 -0700

    prod prep

--- server.js
+++ server.js
@@ -5,19 +5,31 @@ const POKEDEX = require('./pokedex.json')
 app.use(helmet())
 const cors = require('cors')
 
-console.log(process.env.API_TOKEN)
+
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(cors())
 
 app.use((req, res) => {
     res.send('Hello, world!')
 })
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+        response = { error: { message: 'server error' } }
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
+})
+
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`)
+
 })
