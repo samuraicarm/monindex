3bd23b1e9406fa96689b92181bd3a2a928e216cf
Carmen Dukes <carmendukes@gmail.com>
2020-10-03 13:39:54 -0700

    token added

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const POKEDEX = require('./pokedex.json')
+app.use(helmet())
+const cors = require('cors')
+
+console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(cors())
 
 app.use((req, res) => {
     res.send('Hello, world!')
